fe3a1a48791e8bd96a9fa085210475fb0ce1cf28
Michael George <mdgeorge@cs.cornell.edu>
2019-06-24 10:46:12 -0400

    Created demo class, talked about comments.

---
+++ src/lectures/Demo.java
@@ -0,0 +1,18 @@
+package lectures;
+
+public class Demo {
+
+	/**
+	 * Demonstrates some basic features of Java.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// comment
+		/* 
+		everything here is a comment.
+		 */
+		
+	}
+
+}
