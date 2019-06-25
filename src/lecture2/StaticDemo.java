a0b24af7d87ab9aa855b8bc76fa0a39639fad88c
Michael George <mdgeorge@cs.cornell.edu>
2019-06-25 12:11:01 -0400

    Some comments and cleanup

--- src/lecture2/StaticDemo.java
+++ src/lecture2/StaticDemo.java
@@ -1,14 +1,20 @@
 package lecture2;
 
+/** Demonstrate the difference between static variables and fields */
 public class StaticDemo {
 
+	// x is associated with the class StaticDemo; there is only one x
 	static int x = 0;
+	
+	// y is a field; there is one y inside every StaticDemo object
 	int y = 2;
 	
+	// f is like a field: there is (conceptually) one f inside every StaticDemo object
 	public void f() {
 		
 	}
 	
+	// g is like a static variable: there is only one function g, it is associated with the class
 	public static void g() {
 		
 	}
@@ -24,8 +30,11 @@ public class StaticDemo {
 		sd.y = 3;
 		sd.f();
 		
-		StaticDemo.x = 4;
-		StaticDemo.g();
+		// these are technically allowed, but shouldn't be used
+		// because x and g are not part of sd, they are part of StaticDemo
+		// Notice that eclipse gives a warning.
+		sd.x = 4;
+		sd.g();
 		
 		StaticDemo sd2 = new StaticDemo();
 		sd2.y = 5;
@@ -33,5 +42,4 @@ public class StaticDemo {
 		
 		//sd.x?
 	}
-
 }
