2824c74cc4504099bb16ed957a7aec7fa8234b1e
Michael George <mdgeorge@cs.cornell.edu>
2019-06-26 15:38:42 -0400

    Finished lecture; added second representation and class invariants. Ensured that invariants hold.  Checked that tests still pass.

--- src/lecture3/Time.java
+++ src/lecture3/Time.java
@@ -2,26 +2,35 @@ package lecture3;
 
 public class Time {
 	
-	int minute;
-	int hour;
+	// class invariant: in range [0..60*24)
+	// class invariant: minutesSinceMidnight == 60*hour + minute;
+	private int minutesSinceMidnight;
+	
+	// class invariant: in range [0..60)
+	private int minute;
+	
+	// class invariant: in range [0..24)
+	private int hour;
 	
 	/** return the minute within the hour, in [0..60) */
 	public int minute() {
-		return minute;
+		return minutesSinceMidnight % 60;
 	}
 	
 	/** return the hour, in [0..24) */
 	public int hour() {
 		// same as "return hour"
-		return this.hour;
+		return this.minutesSinceMidnight / 60;
 	}
 	
 	/** update this object changing the minute to m
 	 *  
 	 * @param m the new minutes, in range [0..60) // precondition
 	 */
-	public void setMinute(int m) {
-		this.minute = m;
+	public void setMinute(int minute) {
+		assert minute >= 0 && minute < 60;
+		this.minute = minute;
+		this.minutesSinceMidnight = 60*this.hour + minute;
 	}
 	
 	/** update this object, changing the hour to h
@@ -29,6 +38,8 @@ public class Time {
 	 * @param h the new hour, in range [0..24)
 	 */
 	public void setHour(int h) {
+		assert h >= 0 && h < 24;
 		this.hour = h;
+		this.minutesSinceMidnight = this.minute() + 60*h;
 	}
 }
