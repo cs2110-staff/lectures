f5b9f036e8eac91336fa0635f36254a22aed39c0
Michael George <mdgeorge@cs.cornell.edu>
2019-07-18 13:20:52 -0400

    Lecture 17: added mergesort.
    After lecture I added some tests; they all passed (first time)

--- src/lecture16/Sorting.java
+++ src/lecture16/Sorting.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -128,6 +129,78 @@ public class Sorting<E> {
 		return min;
 	}
 	
+	void mergeSort(List<E> a) {
+		mergeSort(a,0,a.size());
+	}
+	
+	/** sort a[start..end) and leave rest of a alone
+	 * post condition:
+	 *            start         end
+	 * a: [      | sorted      |      ]
+	 * 
+	 * @param a
+	 * @param start
+	 * @param end
+	 */
+	void mergeSort(List<E> a, int start, int end) {
+		if (end - start <= 1)
+			return;
+		
+		int mid = (end + start)/2;
+		
+		mergeSort(a,start,mid);
+		mergeSort(a,mid,end);
+		
+		//          start       mid        end
+		// a: [    |  sorted   |  sorted  |     ]
+		
+		merge(a, start, mid, end);
+	}
+	
+	/**
+	 * Precondition:         start      mid        end
+	 *               a: [   |  sorted  |  sorted  |       ]
+	 *               
+	 * Postcondition:        start                 end
+	 *               a: [   |     sorted          |       ]
+	 * 
+	 */
+	void merge(List<E> a, int start, int mid, int end) {
+		List<E> result = new ArrayList<E>();
+		
+		// invariant: result is sorted
+		//            start       i                   mid          j                    end
+		//   a:  [   | in result | sorted, >= result | in result  |  sorted, >= result |     ] 
+		
+		int i = start; int j = mid;
+		
+		while (i != mid || j != end) {
+			if (i == mid) {
+				result.add(a.get(j));
+				j++;
+			}
+			else if (j == end) {
+				result.add(a.get(i));
+				i++;
+			}
+			else if (compare(a,i,j) < 0) {
+				result.add(a.get(i));
+				i++;
+			}
+			else {
+				result.add(a.get(j));
+				j++;
+			}
+		}
+		
+		// a: [   |  in result   |  in result  |   ]
+		
+		// copy result back into a
+		for(int k = 0; k < result.size(); k++) {
+			a.set(start+k, result.get(k));
+		}
+	}
+	
 	// Testing
 	
 	public static class Tests {
@@ -181,6 +254,26 @@ public class Sorting<E> {
 			assertEquals(0, a.get(s.indexOfMin(a, 0, a.size())));
 			assertEquals(3, a.get(s.indexOfMin(a, 2, 5)));
 		}
+		
+		@Test
+		public void testMerge() {
+			Sorting<Integer> s = sorter();
+			//                                 0 1 2  3 4 5 6 7  8 9
+			//                                [      |     |    |   ]
+			List<Integer>    a = Arrays.asList(1,0,7 ,3,6,8,2,7 ,0,7);
+			List<Integer>  exp = Arrays.asList(1,0,7 ,2,3,6,7,8 ,0,7);
+			s.merge(a, 3, 6, 8);
+			assertEquals(exp,a);
+		}
+		
+		@Test
+		public void testMergeSort() {
+			Sorting<Integer> s = sorter();
+			List<Integer>    a = testCase();
+			s.mergeSort(a);
+			assertEquals(testCaseSorted(), a);			
+		}
+		
 		@Test
 		public void testSelectionSort() {
 			Sorting<Integer> s = sorter();
