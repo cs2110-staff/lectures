b5c8cb43a50be187ad9a5f4b9d84e3190600e6e0
Michael George <mdgeorge@cs.cornell.edu>
2019-07-30 12:10:39 -0400

    Lecture 25

---
+++ src/lecture25/ThreadDemo.java
@@ -0,0 +1,85 @@
+package lecture25;
+
+public class ThreadDemo {
+	public static class ExampleThread implements Runnable {
+		private int id;
+		
+		public ExampleThread(int id) {
+			this.id = id;
+		}
+		
+		@Override
+		public void run() {
+			for (int i = 0; true; i++) {
+				System.out.println("Thread " + id + " iteration " + i);
+			}
+		}
+	}
+	
+	public static void runExample() {
+		new Thread(new ExampleThread(1)).start();
+		new Thread(new ExampleThread(2)).start();
+		new Thread(new ExampleThread(3)).start();
+	}
+	
+	public static class ExampleTask implements Runnable {
+		int result = 0;
+		int id;
+		
+		public ExampleTask(int id) {
+			this.id = id;
+		}
+		
+		@Override
+		public void run() {
+			int result = 0;
+			for (int i = 0; i < 10; i++) {
+				System.out.println("task " + id + " running");
+				result += i*i;
+			}
+			this.result = result;
+		}
+	}
+	
+	public static void forkJoin() throws InterruptedException {
+		ExampleTask[] tasks   = new ExampleTask[5];
+		Thread[]      threads = new Thread[5];
+		
+		// fork off threads to execute tasks
+		for (int i = 0; i < 5; i++) {
+			tasks[i] = new ExampleTask(i);
+			threads[i] = new Thread(tasks[i]);
+			threads[i].start();
+		}
+		
+		// all threads started, wait for each to finish
+		for (int i = 0; i < 5; i++) {
+			threads[i].join();
+			System.out.println("thread " + i + " done; result = " + tasks[i].result);
+		}
+	}
+	
+	public static class Race implements Runnable {
+		static int counter = 0;
+		@Override
+		public void run() {
+			for (int i = 0; i < 10000; i++) {
+				counter++;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread[] threads = new Thread[5];
+		for (int i = 0; i < 5; i++) {
+			threads[i] = new Thread(new Race());
+			threads[i].start();
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			threads[i].join();
+		}
+		
+		System.out.println(Race.counter);
+	}
+}
